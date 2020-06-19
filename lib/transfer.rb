32df66d3d3b1d6e30365608379d04b5a863cfeff
Bobby Neal <rsneal01@gmail.com>
2020-06-19 16:41:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,32 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :sender, :receiver, :status, :amount
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+  
+  def valid?
+    if sender.valid? && receiver.valid?
+      true
+    else
+      false
+    end
+  end
+  # how does our code know to go into BankAccount class here?
+  
+  def execute_transaction
+    if sender.valid? && receiver.valid? && self.status == "pending" && sender.balance > amount
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+  
 end
